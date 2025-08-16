1229849641a8d678afea5deef2375d045344666a
va3hr <101849826+va3hr@users.noreply.github.com>
2025-08-16 13:01:15 -0400

    Update MainForm.cs

--- src/MainForm.cs
+++ src/MainForm.cs
@@ -1,14 +1,25 @@
-using System;
-using System.Windows.Forms;
-
 namespace GE_Ranger_Programmer
 {
     public partial class MainForm : Form
     {
         public MainForm()
         {
-            InitializeComponent();
-            // Your UI code here
+            // Initialize components manually
+            this.SuspendLayout();
+            
+            // Add basic controls
+            this.Text = "GE Ranger Programmer";
+            this.ClientSize = new Size(800, 600);
+            
+            var statusLabel = new Label
+            {
+                Text = "Ready to program X2212",
+                Location = new Point(10, 10),
+                AutoSize = true
+            };
+            this.Controls.Add(statusLabel);
+            
+            this.ResumeLayout(false);
         }
     }
 }
