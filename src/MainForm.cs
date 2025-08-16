7cc1c311dd0c8e9928b5543526ae1c38d653e6a2
va3hr <101849826+va3hr@users.noreply.github.com>
2025-08-16 11:53:34 -0400

    Update MainForm.cs

--- src/MainForm.cs
+++ src/MainForm.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Windows.Forms;
 
+namespace GE_Ranger_Programmer
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+            // Your UI code here
+        }
+    }
+}
