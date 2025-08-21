feca09ff386b42399d144f7e26a9d50eac2b7f43
va3hr <101849826+va3hr@users.noreply.github.com>
2025-08-21 13:02:00 -0400

    Add files via upload

--- src/ToneLock.cs
+++ src/ToneLock.cs
@@ -9,6 +9,7 @@ namespace RangrApp.Locked
     public static class ToneLock
     {
         public const string ToneNameNull = "0";
+        public static string ToneNameNullProp => ToneNameNull;
         public static readonly string[] CgTx = new [] { "0","67.0","71.9","74.4","77.0","79.7","82.5","85.4","88.5","91.5","94.8","97.4","100.0","103.5","107.2","110.9","114.8","118.8","123.0","127.3","131.8","136.5","141.3","146.2","151.4","156.7","162.2","167.9","173.8","179.9","186.2","192.8","203.5","210.7" };
 
         private static readonly int[] ScreenToFileBlock =
