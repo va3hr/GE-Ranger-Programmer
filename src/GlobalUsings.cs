9b1a0d33dd9d79a8b46531d1fb90d38ebd2aa4d6
va3hr <101849826+va3hr@users.noreply.github.com>
2025-08-22 11:46:30 -0400

    Add files via upload

--- src/GlobalUsings.cs
+++ src/GlobalUsings.cs
@@ -1,7 +1,6 @@
-// Global framework usings only. No project-local namespaces here.
 global using System;
 global using System.IO;
 global using System.Linq;
 global using System.Collections.Generic;
-global using System.ComponentModel;
+global using System.Drawing;
 global using System.Windows.Forms;
