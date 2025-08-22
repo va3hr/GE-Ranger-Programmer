00f75f7955a9c94a0392e4ce7e5781f98f65ee80
va3hr <101849826+va3hr@users.noreply.github.com>
2025-08-22 09:58:12 -0400

    Add files via upload

--- src/GlobalUsings.cs
+++ src/GlobalUsings.cs
@@ -1,5 +1,13 @@
 global using System;
 global using System.Collections.Generic;
-global using System.Linq;
+global using System.Diagnostics;
+global using System.Drawing;
+global using System.Globalization;
 global using System.IO;
+global using System.Linq;
+global using System.Text;
 global using System.Windows.Forms;
+
+// Project namespaces
+global using RangrApp.Locked;
+global using RangrApp.Codec;
