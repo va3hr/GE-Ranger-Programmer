86a6f740c70575ccf27bfc514fa600f786dbcb31
va3hr <101849826+va3hr@users.noreply.github.com>
2025-08-22 10:36:29 -0400

    Add files via upload

--- src/GlobalUsings.cs
+++ src/GlobalUsings.cs
@@ -1,13 +1,11 @@
+// Minimal global usings for GE-Ranger-Programmer.
+// Remove project-specific namespaces that caused CS0246 ('RangrApp' not found).
+
 global using System;
-global using System.Collections.Generic;
-global using System.Diagnostics;
-global using System.Drawing;
-global using System.Globalization;
 global using System.IO;
 global using System.Linq;
 global using System.Text;
+global using System.Collections.Generic;
+global using System.Globalization;
+global using System.Drawing;
 global using System.Windows.Forms;
-
-// Project namespaces
-global using RangrApp.Locked;
-global using RangrApp.Codec;
