4a332f45c17fc4e4314a4f878c479d7599089041
va3hr <101849826+va3hr@users.noreply.github.com>
2025-08-26 23:01:14 -0400

    Add files via upload

--- src/GlobalUsings.cs
+++ src/GlobalUsings.cs
@@ -1,1 +1,9 @@
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Text;
+global using System.Threading.Tasks;
+global using System.Windows.Forms;
+
+// Ensure the unqualified 'ToneLock' in MainForm/RgrCodec resolves to the namespaced class
 global using RangrApp.Locked;
