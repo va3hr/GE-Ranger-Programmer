47254be531c0c3a262615bc50426047fad7d1b65
va3hr <101849826+va3hr@users.noreply.github.com>
2025-09-05 00:50:51 -0400

    Update MainForm.cs

--- src/MainForm.cs
+++ src/MainForm.cs
@@ -1,1 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
 
+namespace GE_Ranger_Programmer
+{
+    public class ToneDecoder
+    {
+        // ... The full, correct ToneDecoder code ...
+    }
+}
