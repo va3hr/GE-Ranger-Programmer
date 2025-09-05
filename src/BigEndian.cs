a03d87c6402564acd27e71df0ee66a51e095657b
va3hr <101849826+va3hr@users.noreply.github.com>
2025-09-05 01:47:16 -0400

    Update BigEndian.cs

--- src/BigEndian.cs
+++ src/BigEndian.cs
@@ -1,7 +1,32 @@
-namespace GE_Ranger_Programmer
+namespace GE_Ranger_Programnamespace GE_Ranger_Programmer
 {
     public static class BigEndian
     {
+        // This is the method your RgrCodec.cs needs.
+        public static byte BitMsb(byte value)
+        {
+            byte result = 0;
+            for (int i = 0; i < 8; i++)
+            {
+                if ((value & (1 << i)) != 0)
+                {
+                    result |= (byte)(1 << (7 - i));
+                }
+            }
+            return result;
+        }
+
+        public static ushort SwapUInt16(ushort value)
+        {
+            return (ushort)(((value & 0xFF) << 8) | ((value >> 8) & 0xFF));
+        }
+
+        public static short SwapInt16(short value)
+        {
+            return (short)SwapUInt16((ushort)value);
+        }
+        
+        // This is the method we have been using in MainForm.cs
         public static byte[] SwapBytes(byte[] data)
         {
             byte[] swapped = new byte[data.Length];
